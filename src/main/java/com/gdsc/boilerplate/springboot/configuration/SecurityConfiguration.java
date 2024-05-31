c97b11d0c94acd4d26df891fc6caa6e91322e954
hungpham28 <hungpham28092003@gmail.com>
2024-05-31 21:28:20 +0700

    improve: change path swagger to /api-docs

--- src/main/java/com/gdsc/boilerplate/springboot/configuration/SecurityConfiguration.java
+++ src/main/java/com/gdsc/boilerplate/springboot/configuration/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
 		return http.cors().and().csrf().disable()
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
-				.antMatchers("/register", "/login","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/actuator/**").permitAll()
+				.antMatchers("/register", "/login","/v3/api-docs/**", "/swagger-ui/**", "/api-docs", "/actuator/**").permitAll()
 				.anyRequest().authenticated().and()
 				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
