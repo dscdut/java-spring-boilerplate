c0fd04073589fe9e5c38f5206c2a07f57728cbec
ThuongHoai081 <th316942@gmail.com>
2024-06-11 06:21:29 +0700

    db(GBB-49): add role table

---
+++ src/main/java/com/gdsc/boilerplate/springboot/configuration/DataInitialization.java
@@ -0,0 +1,34 @@
+package com.gdsc.boilerplate.springboot.configuration;
+
+
+import com.gdsc.boilerplate.springboot.model.Role;
+import com.gdsc.boilerplate.springboot.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class DataInitialization {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public DataInitialization(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @PostConstruct
+    public void initData() {
+        createRoleIfNotExists("ADMIN");
+        createRoleIfNotExists("USER");
+    }
+
+    private void createRoleIfNotExists(String userRole) {
+        Role role = roleRepository.findByName(userRole);
+        if (role == null) {
+            role = Role.builder().name(userRole).build();
+            roleRepository.save(role);
+        }
+    }
+}
