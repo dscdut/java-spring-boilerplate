c17f5d7a1dceca2eacf68383815679a2301cb873
ThuongHoai081 <th316942@gmail.com>
2024-06-11 06:21:06 +0700

    fix(GBB-49): fix sercurity

---
+++ src/main/java/com/gdsc/boilerplate/springboot/security/dto/UserUpdateInformationRequest.java
@@ -0,0 +1,29 @@
+package com.gdsc.boilerplate.springboot.security.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserUpdateInformationRequest {
+
+    @Pattern(regexp = "^[a-zA-Z]+([ '-][a-zA-Z]+)*$", message = "{fullname_invalid}")
+    @NotEmpty(message = "{fullname_not_empty}")
+    @Schema( example = "string")
+    private String full_name;
+
+    @NotEmpty(message = "{email_not_empty}")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "{email_invalid}")
+    @Schema( example = "string")
+    private String email;
+
+}
